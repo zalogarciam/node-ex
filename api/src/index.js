ea38063af134cd7962383b7298b04b85357f328e
zalogarciam <gegarciam95@gmail.com>
2023-04-14 13:50:19 -0500

    Add routers in server express

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const server = express();
 
-server.use(express.json(), express.urlencoded())
+server.use(express.json(), express.urlencoded());
 
 const products = [
   {
@@ -24,17 +24,37 @@ const products = [
 
 const PORT = 3000;
 
-server.get("/productos", (req, res) => {
-  res.status(200).json({
-    content: products,
+server
+  .route("/productos")
+  .get((req, res) => {
+    res.status(200).json({
+      content: products,
+    });
+  })
+  .post((req, res) => {
+    if (Object.keys(req.body).length === 0) {
+      res.status(400).json({
+        message: "Incorrect",
+      });
+    }
+    res.status(201).json({
+      message: "Product was added",
+      content: req.body,
+    });
   });
-});
 
-server.post("/productos", (req, res) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Product was added",
-  });
+server.route("/producto/:id").get((req, res) => {
+  const { id } = req.params;
+  const resultado = products[id];
+  if (resultado) {
+    res.status(200).json({
+      content: resultado,
+    });
+  } else {
+    res.status(404).json({
+      content: "No existe",
+    });
+  }
 });
 
 server.listen(PORT, () => {
