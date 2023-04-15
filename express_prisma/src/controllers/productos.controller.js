97c39efb3c6a32a0a7f659043e488ec0231a2502
zalogarciam <gegarciam95@gmail.com>
2023-04-15 10:28:08 -0500

    Handle when category does not exists

--- express_prisma/src/controllers/productos.controller.js
+++ express_prisma/src/controllers/productos.controller.js
@@ -3,12 +3,22 @@ import { productoRouter } from "../routes/productos.routes.js";
 
 export const crearProducto = async (req, res) => {
   const data = req.body;
+  console.log(data);
+
+  const categoria = await Prisma.categoria.findFirst({
+    where: { id: data.categoriaId },
+    select: { id: true },
+  });
+
+  if (!categoria) {
+    return res.status(400).json({
+      message: "Categoria no existe",
+    });
+  }
 
   const resultado = await Prisma.producto.create({
     data: { ...data, fechaVencimiento: new Date(data.fechaVencimiento) },
   });
-
-  console.log(resultado);
 
   res.status(201).json({
     message: "El producto se creo exitosamente",
